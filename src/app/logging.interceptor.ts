f899e00cd3d705dbdd497330ceaf508b74debc17
Vivekanand P V <vivekanandpv@outlook.com>
2020-09-11 22:55:10 +0530

    done

--- src/app/logging.interceptor.ts
+++ src/app/logging.interceptor.ts
@@ -6,6 +6,7 @@ import {
   HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { finalize, tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements HttpInterceptor {
@@ -15,7 +16,12 @@ export class LoggingInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
+    const started = Date.now();
     console.log('Logging Interceptor', request.url);
-    return next.handle(request);
+    return next.handle(request).pipe(
+      tap((e) => {
+        console.log('Response e', e);
+      })
+    );
   }
 }
