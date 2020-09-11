fb367545ca0ad4f08ca14102836ca6fb3d61f9f4
Vivekanand P V <vivekanandpv@outlook.com>
2020-09-11 22:45:10 +0530

    done

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -16,6 +16,14 @@ export class AuthInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
     console.log('Auth Interceptor', request.url);
-    return next.handle(request);
+
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
+    const authReq = request.clone({
+      headers: request.headers.set('Bearer', token),
+    });
+
+    return next.handle(authReq);
   }
 }
